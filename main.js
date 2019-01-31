2fadea3e21462eaddb653584dc80bfc19f7157c0
ThisIsNotAFakeAccount <47157298+ThisIsNotAFakeAccount@users.noreply.github.com>
2019-01-30 18:06:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
 try {  // please help complete this block :')
-throw "wew";
+  throw "wew";
 
-console.log = (...args) => { args.unshift('😠'); console.error(...args); };
-process.on('uncaughtException', err => {}); // uber error handler
-function hello_darkness(animal){
-  console.log("my old " + animal);
+  console.log = (...args) => { args.unshift('😠'); console.error(...args); };
+  process.on('uncaughtException', err => {}); // uber error handler
+  function hello_darkness(animal){
+    console.log("my old " + animal);
+  }
+
+  function main() {
+    var heyguys = "why JavaScript?";
+    var chungus = "Big Chungus";
+    hello_darness(chungus)
+    return 'uwu';
+  }
+
+  function tell_joke(theme) {
+    var start = "knock knock";
+    return this;
+  }
+
+  const thisEvalutatesTo10DontAskMeWhy = [+!+[]]+[+[]];
+  const isJavascript = ![] == []
+  var 𝓋𝒶𝓇 = "🍪 ⋆ 🍡  🎀  Ѷ𝓐Ⓡ 🎀  🍡 ⋆ 🍪";
+
+  setInterval(   () => { setTimeout( () => { throw new Error("KEEP ROLLIN ROLLLIN~~~");}) }, 2019.1);
+} catch (e) {
+  
 }
-
-function main() {
-  var heyguys = "why JavaScript?";
-  var chungus = "Big Chungus";
-  hello_darness(chungus)
-  return 'uwu';
-}
-
-function tell_joke(theme) {
-  var start = "knock knock";
-  return this;
-}
-
-const thisEvalutatesTo10DontAskMeWhy = [+!+[]]+[+[]];
-const isJavascript = ![] == []
-var 𝓋𝒶𝓇 = "🍪 ⋆ 🍡  🎀  Ѷ𝓐Ⓡ 🎀  🍡 ⋆ 🍪";
-
-setInterval(   () => { setTimeout( () => { throw new Error("KEEP ROLLIN ROLLLIN~~~");}) }, 2019.1);
 
 main();
